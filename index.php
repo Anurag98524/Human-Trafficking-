8e0e7fafd04626af1d010ef6acb5d5f199710b20
Anurag kumar gupta <48939698+Anurag98524@users.noreply.github.com>
2020-04-24 13:13:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,320 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Human Trafficking</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    
+	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index.php">Human <span>Trafficking</span></a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="#about" class="nav-link">About</a></li>
+                <li class="nav-item"><a href="news.php" class="nav-link">News</a></li>
+	          <li class="nav-item"><a href="missing.php" class="nav-link">Missing</a></li>
+	          <li class="nav-item"><a href="contact.html" class="nav-link">Feedback</a></li>
+                <li class="nav-item"><a href="survey.php" class="nav-link">Survey</a></li>
+				<li class="nav-item"><a href="admin.php" class="nav-link">NGO</a></li>
+                <li class="nav-item"><a href="admin.php" class="nav-link">Admin</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+    
+    <div class="hero-wrap" style="background-image: url('images/bg3.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text justify-content-start align-items-center">
+          <div class="col-lg-6 col-md-6 ftco-animate d-flex align-items-end">
+          	<div class="text">
+	            <h1 class="mb-4">Human<span> Trafficking is modern day </span> <span>slavery</span></h1>
+	            <p style="font-size: 18px;">Human Trafficking is modern day slavery. Two principal causes for trafficking are poverty, which lead to exploitation, and criminality. Generally, agents give them tempting offers of money or having urban lifestyle and then they do trading.</p>
+	            <a href="web/cemara.html" class="icon-wrap popup-vimeo d-flex align-items-center mt-4">
+	            	<div class="icon d-flex align-items-center justify-content-center">
+	            		<span class="ion-ios-play"></span>
+	            	</div>
+	            	<div class="heading-title ml-5">
+		            	<span>Instant Report</span>
+	            	</div>
+	            </a>
+            </div>
+          </div>
+          <div class="col-lg-2 col"></div>
+          <div class="col-lg-4 col-md-6 mt-0 mt-md-5 d-flex">
+          	<form method="post" action="upload.php" class="request-form ftco-animate"  enctype="multipart/form-data">
+          		<h2>Query</h2>
+	    				<div class="form-group">
+	    					<label for="" class="label">Name</label>
+	    					<input type="text"  name="name" class="form-control" placeholder="Please types name...">
+	    				</div>
+	    				<div class="form-group">
+	    					<label for="" class="label" >Contact Number</label>
+	    					<input type="text" name="contact" class="form-control" placeholder="Contact number...">
+	    				</div>
+				       
+	    				<div class="d-flex">
+	    					<div class="form-group mr-2">
+	                <label for="" class="label" >Date</label>
+	                <input type="text" class="form-control" name="date" id="book_pick_date" placeholder="Date">
+	              </div>
+	              <div class="form-group">
+                <label for="" class="label" >Time</label>
+                <input type="text" class="form-control" name="time" id="time_pick" placeholder="Time">
+              </div>
+              </div>
+             <div class="form-group">
+	    					<label for="" class="label" >Message</label>
+	    					<input type="text" name="message" class="form-control" placeholder="Message related to human traffiking.">
+	    				</div>
+	            <div class="form-group">
+	              <input type="submit" value="Submit" name="submit" class="btn btn-primary py-3 px-4">
+	            </div>
+	    			</form>
+          </div>
+            
+        </div>
+      </div>
+    </div>
+
+   
+    <section class="ftco-section services-section ftco-no-pt ftco-no-pb">
+      <div class="container">
+      	<div class="row justify-content-center">
+          <div class="col-md-12 heading-section text-center ftco-animate mb-5">
+          	<br/><br/>
+            <h2 class="mb-2">Our Services</h2>
+          </div>
+        </div>
+        <div class="row d-flex">
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services">
+              <div class="media-body py-md-4">
+              	<div class="d-flex mb-3 align-items-center">
+	              	<div class="icon"><span class="flaticon-customer-support"></span></div>
+	                <h3 class="heading mb-0 pl-3">24/7 Call Support</h3>
+                </div>
+                <p>
+You can call anywhere from human trafiking for related suggestions and advice, we will be happy to talk to you.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services">
+              <div class="media-body py-md-4">
+              	<div class="d-flex mb-3 align-items-center">
+	              	<div class="icon"><span class="flaticon-route"></span></div>
+	                <h3 class="heading mb-0 pl-3">Any location</h3>
+                </div>
+                <p>You can use this portal from anywhere to prevent human trafficking due to its a crime.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services">
+              <div class="media-body py-md-4">
+              	<div class="d-flex mb-3 align-items-center">
+	              	<div class="icon"><span class="flaticon-online-booking"></span></div>
+	                <h3 class="heading mb-0 pl-3">GPS Tracker</h3>
+                </div>
+                <p>We will try to reach you as soon as possible so that we can help you and save the country from human trafficking</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services">
+              <div class="media-body py-md-4">
+              	<div class="d-flex mb-3 align-items-center">
+	              	<div class="icon"><span class="flaticon-rent"></span></div>
+	                <h3 class="heading mb-0 pl-3">Instant Report</h3>
+                </div>
+                <p>You can tell us anytime anywhere through instant report regarding human trafficking, we will take action on it soon.
+</p>
+              </div>
+            </div>      
+          </div>
+        </div>
+      </div>
+    </section>
+<br/><br>
+    
+    <section class="ftco-section services-section img" style="background-image: url(images/bg2.jpg);">
+    	<div class="overlay"></div>
+    	<div class="container">
+    		<div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center heading-section heading-section-white ftco-animate">
+          	
+            <h2 class="mb-3" id="work" >How it works</h2>
+          </div>
+        </div>
+    		<div class="row">
+    			<div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services services-2">
+              <div class="media-body py-md-4 text-center">
+              	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-route"></span></div>
+                <h3>Choose Instant Report</h3>
+                <p>You can call anywhere and capture image relate to traffikng from human trafiking for related suggestions and advice.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services services-2">
+              <div class="media-body py-md-4 text-center">
+              	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-select"></span></div>
+                <h3>Writte Report</h3>
+                <p>If any body can be drop a message with portal for related to humman traffkng</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services services-2">
+              <div class="media-body py-md-4 text-center">
+              	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-rent"></span></div>
+                <h3>By Call</h3>
+                <p>Any body directaly to call with the help of given number n my portal related to human Traffiking</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services services-2">
+              <div class="media-body py-md-4 text-center">
+              	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-review"></span></div>
+                <h3>NGO's</h3>
+                <p> Listing of NGO's and Governing Body (eg: CID, UNICEF etc.) for focuss on message</p>
+              </div>
+            </div>      
+          </div>
+    		</div>
+    	</div>
+    </section>
+
+  <br/><br/>
+		<section class="ftco-section ftco-no-pt ftco-no-pb" id="about">
+			<div class="container">
+				<div class="row no-gutters">
+					<div class="col-md-6 p-md-5 img img-2 d-flex justify-content-center align-items-center" style="background-image: url(images/bg1.jpg);">
+					</div>
+					<div class="col-md-6 wrap-about py-md-5 ftco-animate">
+	          <div class="heading-section mb-5 pl-md-5">
+	          	<span class="subheading">About us</span>
+	            <h2 class="mb-4">Human Trafficking</h2>
+
+	            <p>Human Trafficking is modern day slavery. Two principal causes for trafficking are poverty, which lead to exploitation, and criminality. Generally, agents give them tempting offers of money or having urban lifestyle and then they do trading.  </p>
+	            <p>Human Trafficking is modern day slavery. Two principal causes for trafficking are poverty, which lead to exploitation, and criminality. Generally, agents give them tempting offers of money or having urban lifestyle and then they do trading. As per govt. report, 2700 person trafficked annually from Bihar.<br>In children trafficking cases, parents say that they were not aware about putting their children into hand of human traffickers.</p>
+	            <p><a href="#" class="btn btn-primary">More..</a></p>
+	          </div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+   
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">About Human Trafficking</h2>
+              <p>which lead to exploitation, and criminality. Generally, agents give them tempting offers of money or having urban lifestyle and then they do trading. </p>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+              
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-5">
+              <h2 class="ftco-heading-2">Information</h2>
+              <ul class="list-unstyled">
+                <li><a href="#about" class="py-2 d-block">About</a></li>
+                <li><a href="#work" class="py-2 d-block">How It work</a></li>
+                
+                
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Any Quary</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Mzaffarpur Institute of Techology, Muzaffarpur</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+91 987654321 </span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">human@code4bihar.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p><!--anuragb kmar gupta  -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Human Trafficking  by MIT
+  </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
